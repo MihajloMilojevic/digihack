88e5e69c8b1013d4765852d15490b56f9b481a53
nickola23 <nikolamilanovic2305@gmail.com>
2024-09-28 22:38:30 +0200

    all pages added

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -1,20 +1,20 @@
 import styles from "./index.module.css";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Map from "../components/Sidebar/Map";
+import Holes from "../components/Sidebar/Holes";
+import News from "../components/Sidebar/News";
+import Streets from "../components/Sidebar/Streets";
+import Stats from "../components/Sidebar/Stats";
 import { useState } from "react";
 
-function Noop() {
-    return <h1>Noop</h1>
-}
 
-
-const components = [Map, Noop, Noop]
+const components = [Map, Holes, News, Streets, Stats]
 
 export default function Admin(){
 
     const [tabIndex, setTabIndex] = useState(0)
     let Component = components[tabIndex]
-    
+
     return(
         <>
             <Sidebar setTabIndex={setTabIndex} />
---
+++ src/components/Sidebar/Streets.jsx
@@ -0,0 +1,23 @@
+import styles from "./Streets.module.css";
+import Street from "../../street/Street"
+
+export default function Streets(){
+    return(
+        <div className={styles.mapMain}>
+            <h1 className={styles.maph1}>Mapa Grada</h1>
+            <div className={styles.mapMainCont}>
+                <div className={styles.mapLeft}>
+                    <div className={styles.maph2}>Broj Aktivnih Vozila</div>
+                    <div className={styles.mapText}>23</div>
+                    <div className={styles.maph2}>Broj Aktivnih Kamera</div>
+                    <div className={styles.mapText}>23</div>
+                    <div className={styles.maph2}>Broj Neispravnih vozila</div>
+                    <div className={styles.mapText}>7</div>
+                </div>
+                <div className={styles.mapRight}>
+                    <Street />
+                </div>
+            </div>
+        </div>
+    );
+}
