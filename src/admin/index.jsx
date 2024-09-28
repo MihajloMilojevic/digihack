2a68c2ff9b1feeb6eb40e015e6c0c15e690c36a8
MihajloMilojevic <milojevicm374@gmail.com>
2024-09-28 19:55:48 +0200

    iframe

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -3,6 +3,9 @@ import Sidebar from "../components/Sidebar/Sidebar";
 
 export default function Admin(){
     return(
+        <>
         <Sidebar />
+        <iframe style={{resize: "both"}} width={500} height={500} src="https://editor.p5js.org/MihajloMilojevic/full/M66S-GihY"></iframe>
+        </>
     );
 }
