fa50379b0fb6d385034dd720eac466aef92ec323
nickola23 <nikolamilanovic2305@gmail.com>
2024-09-29 07:24:36 +0200

    obavestenja design ++

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -5,11 +5,15 @@ export default function News(){
         <div className={styles.mapMain}>
             <h1 className={styles.maph1}>Obavestenja</h1>
             <div className={styles.mapMainCont}>
-                <div className={styles.mapLeft}>
-                    
-                </div>
-                <div className={styles.mapRight}>
-                    
+                
+            </div>
+            <div className={styles.newsMain}>
+                <div className={styles.newsCont}>
+                    <div className={styles.newsLeft}>
+                        <div className={styles.newsTitle}>Naslov obavestenja</div>
+                        <div className={styles.newsText}>Tekst: izasla je nova rupa idi resi to odmah!</div>
+                    </div>
+                    <div className={styles.newsRight}><div className={styles.newsRightDot}></div></div>
                 </div>
             </div>
         </div>
